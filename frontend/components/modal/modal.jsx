6320429cc1c572430d16aaa31b49bb1bde6c19b0
Gary <gdominguez210@live.com>
2019-10-04 15:46:20 -0400

    Updated close modal icon

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,49 +1,50 @@
-import React from 'react';
-import {closeModal} from '../../actions/modal_actions';
-import {connect} from 'react-redux';
-import LoginFormContainer from '../session/LoginFormContainer.jsx'
-import SignUpFormContainer from '../session/SignUpFormContainer.jsx'
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; 
-import { CSSTransition } from 'react-transition-group';
-const Modal = (props) => {
-    debugger
-    if (!props.modal) {
-        return null;
-    }
+import React from "react";
+import { closeModal } from "../../actions/modal_actions";
+import { connect } from "react-redux";
+import LoginFormContainer from "../session/LoginFormContainer.jsx";
+import SignUpFormContainer from "../session/SignUpFormContainer.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { CSSTransition } from "react-transition-group";
+const Modal = props => {
+  debugger;
+  if (!props.modal) {
+    return null;
+  }
 
-    let component = null;
+  let component = null;
 
-    switch(props.modal) {
+  switch (props.modal) {
+    case "login":
+      debugger;
+      component = <LoginFormContainer />;
+      break;
+    case "signup":
+      debugger;
+      component = <SignUpFormContainer />;
+      break;
+    default:
+      return null;
+  }
 
-        case 'login':
-            debugger
-            component = <LoginFormContainer />;
-            break;
-        case 'signup':
-            debugger
-            component = <SignUpFormContainer />;
-            break;
-        default:
-            return null;
-    }
-
-    return (
-        <div className="modal-background" onClick={props.closeModal}>
-            <div className="modal-child" onClick={e => e.stopPropagation()}>
-                { component }
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="modal-background" onClick={props.closeModal}>
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        {component}
+      </div>
+    </div>
+  );
+};
 
 const msp = state => ({
-        modal: state.ui.modal
-    }
-);
-
-const mdp = dispatch => ({
-    closeModal: () => dispatch(closeModal())
+  modal: state.ui.modal
 });
 
+const mdp = dispatch => ({
+  closeModal: () => dispatch(closeModal())
+});
 
-export default connect(msp, mdp)(Modal);
+export default connect(
+  msp,
+  mdp
+)(Modal);
--- frontend/components/session/SessionForm.jsx
+++ frontend/components/session/SessionForm.jsx
@@ -1,102 +1,122 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class SessionForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: ""
-        };
-        debugger
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.update = this.update.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    debugger;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        debugger
-        const user = Object.assign({}, this.state);
-        this.props.processForm(user).then(this.props.closeModal);
-        // this.props.history.push("/");
+  handleSubmit(e) {
+    e.preventDefault();
+    debugger;
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user).then(this.props.closeModal);
+    // this.props.history.push("/");
+  }
 
-    }
+  update(field) {
+    return e => {
+      this.setState({
+        [field]: e.target.value
+      });
+    };
+  }
 
-    update(field){
-        return(e)=>{this.setState({
-            [field]: e.target.value
-        })}
+  renderErrors() {
+    const errors = this.props.errors || [];
+    debugger;
+    return (
+      <ul>
+        {errors.map((error, i) => (
+          <li key={`error-${i}`}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
+  demoUser() {
+    const demo_user = { username: "Demo", password: "test123" };
+    return (
+      <button
+        className="readon-outline"
+        onClick={() => this.props.demo(demo_user).then(this.props.closeModal)}
+      >
+        <p>
+          <i className="far fa-user"></i> Continue as Demo User
+        </p>
+      </button>
+    );
+  }
+  header() {
+    debugger;
+    if (this.props.formType === "Signup") {
+      return <h2>Welcome to NeverWait</h2>;
+    } else if (this.props.formType === "Login") {
+      return <h2>Please Sign In</h2>;
     }
+  }
 
-    renderErrors(){
-        const errors = this.props.errors || [];
-        debugger
-        return(
-            <ul>
-                {errors.map((error, i)=>(
-                    <li key={`error-${i}`}>{error}</li>
-                    ))}
-            </ul>
-        );
+  footer() {
+    if (this.props.formType === "Login") {
+      return (
+        <div class="form-footer">
+          <p>New to NeverWait? {this.props.otherForm}</p>
+        </div>
+      );
     }
-    demoUser(){
-        const demo_user = {username: 'Demo', password:'test123'}
-        return(
-            <button className="readon-outline" onClick={() => this.props.demo(demo_user).then(this.props.closeModal)}><p><i className="far fa-user"></i> Continue as Demo User</p></button>
-            )
-    }
-    header(){
-        debugger
-        if (this.props.formType === 'Signup'){
-            return (
-                <h2>Welcome to NeverWait</h2>
-            )
-        } else if(this.props.formType === 'Login'){
-            return (
-                <h2>Please Sign In</h2>
-            )
-        }
-    }
+  }
 
-    footer(){
-        if (this.props.formType === 'Login') {
-            return (
-                <div class="form-footer">
-                <p>New to NeverWait? {this.props.otherForm}</p>
-                </div>
-            )
-        }
-    }
-
-    render(){
-        const errors = this.props.errors;
-        debugger
-        const errorItems = errors.map(error=>{return (<li key={error.id} className="error">{error}</li>)});
-        debugger
-        return(
-            <>
-            <div class="form-container">
-            {this.header()}
-            <hr/>
-            <form onSubmit={this.handleSubmit}>
-            <div onClick={this.props.closeModal} className="close-x">X</div>
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder="Username*" value={this.state.username} onChange={this.update('username')}/>
-                <label htmlFor="password" >Password</label>
-                <input type="password" placeholder="Password*" value={this.state.password} onChange={this.update('password')}/>
-                <ul className="errors">
-                {errorItems}
-                </ul>
-            <button className="readon-submit">{this.props.formType}</button>
-            </form>
-            <hr />
-            <p>Don't want to complete the form?</p>
-            {this.demoUser()}
-            {this.footer()}
+  render() {
+    const errors = this.props.errors;
+    debugger;
+    const errorItems = errors.map(error => {
+      return (
+        <li key={error.id} className="error">
+          {error}
+        </li>
+      );
+    });
+    debugger;
+    return (
+      <>
+        <div class="form-container">
+          {this.header()}
+          <hr />
+          <form onSubmit={this.handleSubmit}>
+            <div onClick={this.props.closeModal} className="close-x">
+              <FontAwesomeIcon icon="times" />
             </div>
-            </>
-        )
-    }
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              placeholder="Username*"
+              value={this.state.username}
+              onChange={this.update("username")}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Password*"
+              value={this.state.password}
+              onChange={this.update("password")}
+            />
+            <ul className="errors">{errorItems}</ul>
+            <button className="readon-submit">{this.props.formType}</button>
+          </form>
+          <hr />
+          <p>Don't want to complete the form?</p>
+          {this.demoUser()}
+          {this.footer()}
+        </div>
+      </>
+    );
+  }
 }
 
 export default withRouter(SessionForm);

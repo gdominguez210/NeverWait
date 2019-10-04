2b3a7e95226fe0242b4dcf41b878b7379f3e7c53
Gary <gdominguez210@live.com>
2019-10-04 16:02:25 -0400

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,32 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # User.delete_all
 # User.new(username: 'Demo', password:'test123').save!
-
-Restaurant.create({name: 'MMMMM GOOD FOOD', address:'200 W 44th, New York, NY 10036', phone:'(212) 221-3800', owner_id: '1', location_id:'1', lat:'40.757506', long:'-73.986686'})
+Restaurant.delete_all
+Restaurant.create({
+ name: "MMMMM GOOD FOOD",
+ address: "200 W 44th, New York, NY 10036",
+ phone: "(212) 221-3800",
+ owner_id: 5,
+ location_id: 1,
+ lat: 0.40757506e2,
+ long: -0.73986686e2,
+ description:
+  "Bacon ipsum dolor amet bacon chuck kielbasa jowl fatback prosciutto short ribs spare ribs ham rump ground round corned beef hamburger doner pork loin. Strip steak pork belly pastrami tenderloin shoulder. Turducken drumstick tongue bresaola buffalo, ground round leberkas. Filet mignon pork chop chuck frankfurter venison pastrami strip steak sausage turkey brisket fatback chicken turducken biltong.",
+ website: "https://allthebacon.com/",
+ price_range: "cheap",
+ capacity: 50,
+ cross_street: nil,
+ neighborhood: "Brooklyn",
+ hours_of_operation:
+  "Breakfast Monday to Friday 8am to11am, Brunch Saturday, and Sunday 9am to 3:45pm, Lunch Monday to Friday 11:30 to 3:30pm, Dinner Sunday to Wednesday 5 pm to 11 pm, Thursday to Saturday 5 pm to 12 Midnight.",
+ cuisines: "BACON",
+ dining_style: "casual",
+ dress_code: "casual",
+ parking_details: nil,
+ public_transit: nil,
+ payment_options: nil,
+ executive_chef: nil,
+ additional: nil,
+ created_at: Wed, 02 Oct 2019 23:02:30 UTC +00:00,
+ updated_at: Fri, 04 Oct 2019 04:17:10 UTC +00:00,
+ featured_img_url: "after-business-hours-architecture-bar.jpg"});

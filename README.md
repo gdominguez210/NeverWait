d045170fc656201007d08e0c8adb88b692f0fe61
Gary <gdominguez210@live.com>
2019-10-11 11:24:28 -0400

    update readme

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
-# README
+# NeverWait
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+[Link to live site](http://neverwait.herokuapp.com/)
+[NeverWait](http://neverwait.herokuapp.com/) is a fullstack application that is modeled after [OpenTable](https://www.opentable.com/). Neverwait utilizes React / Redux to manage the frontend and Ruby on Rails with a PostgresSQL database to store and manage data. Asset management is handled via Amazon Web Services.
 
-Things you may want to cover:
+---
 
-* Ruby version
+## Main Features
 
-* System dependencies
+- User account creation, and login
+- Users can create/update/delete restaurants/reviews on the website.
+- Users can make reservations for restaurants on the website.
 
-* Configuration
+- A Restaurant show page has a stat breakdown with various information such as:
 
-* Database creation
+  - Total average of all reviews on the restaurant
+  - Amount of total reviews
+  - Amount of ratings of each type (1-5)
+  - Percent of reviews that recommend the restuarant
 
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+- The reservation form first prompts the user for a date and time for their reservation.
+  Before prompting the user for more information, the form validates that there is an opening at the requested time. If the validattion succeeds, the form redirects to a landing page with an additional form that prompts the user for more information about their reservation, ie. first name, last name, email, etc. This landing page, prefills the information submitted from the previous form.
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,33 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as APIUtils from './util/session_api_util';
-import {signup, login} from './actions/session_actions';
-import configureStore from './store/store';
-import Root from './components/root';
-import * as RestActions from './actions/restaurants_actions';
-document.addEventListener("DOMContentLoaded", ()=>{
-    let store;
+import React from "react";
+import ReactDOM from "react-dom";
+import * as APIUtils from "./util/session_api_util";
+import { signup, login } from "./actions/session_actions";
+import configureStore from "./store/store";
+import Root from "./components/root";
+import * as RestActions from "./actions/restaurants_actions";
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
 
-    if(window.currentUser){
-        const preloadedState = {
-            entities: {
-                users: {[window.currentUser.id]: window.currentUser}
-            },
-            session: {id: window.currentUser.id}
-        }
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-    } else {
-        store = configureStore();
-    }
-
-    window.login = login;
-    window.logout = APIUtils.logout;
-    window.signup = signup;
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.fetchRestaurants = RestActions.fetchRestaurants;
-    window.fetchRestaurant = RestActions.fetchRestaurant;
-    const root = document.getElementById("root");
-    ReactDOM.render(<Root store={store}/>, root)
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
 });

9df3d94a61df78d1b63f13ef4851792de7660613
Gary <gdominguez210@live.com>
2019-11-13 21:53:10 -0500

    remove error boundaries

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -27,67 +27,65 @@ const App = props => {
   library.add(fas);
   return (
     <>
-      <ErrorBoundary>
-        <ModalContainer />
-        <Header />
-        {/* <Route exact path="/" component={Banner} /> */}
+      <ModalContainer />
+      <Header />
+      {/* <Route exact path="/" component={Banner} /> */}
 
-        {/* <section className="inner-container"> */}
-        <ProtectedRoute
-          exact
-          path="/new-restaurant"
-          component={CreateRestaurantForm}
-        />
-        <ProtectedRoute
-          exact
-          path="/restaurants/:restaurantId/edit"
-          component={EditRestaurantForm}
-        />
-        <ProtectedRoute
-          exact
-          path="/new-reservation"
-          component={CreateReservationForm}
-        />
+      {/* <section className="inner-container"> */}
+      <ProtectedRoute
+        exact
+        path="/new-restaurant"
+        component={CreateRestaurantForm}
+      />
+      <ProtectedRoute
+        exact
+        path="/restaurants/:restaurantId/edit"
+        component={EditRestaurantForm}
+      />
+      <ProtectedRoute
+        exact
+        path="/new-reservation"
+        component={CreateReservationForm}
+      />
 
-        {/* <Route exact path="/" component={RestaurantFeaturedContainer} /> */}
-        <ErrorBoundary>
-          <Route exact path="/" component={Splash} />
-        </ErrorBoundary>
-        {/* <Route exact path="/" component={CarouselContainer} />
+      {/* <Route exact path="/" component={RestaurantFeaturedContainer} /> */}
+
+      <Route exact path="/" component={Splash} />
+
+      {/* <Route exact path="/" component={CarouselContainer} />
         <Route exact path="/" component={Location} /> */}
-        <Route exact path="/restaurants" component={RestaurantIndexContainer} />
-        <ScrollToTop>
-          <Route
-            exact
-            path="/search-restaurants"
-            component={RestaurantSearchContainer}
-          />
-        </ScrollToTop>
-        {/* <Route exact path="/search/:query" component={RestaurantIndexContainer} /> */}
-        <ProtectedRoute
-          exact
-          path="/users/:userId/reservations"
-          component={ReservationIndexContainer}
-        />
-        <ProtectedRoute
-          exact
-          path="/users/:userId/favorites"
-          component={FavoritesIndexContainer}
-        />
-        <ProtectedRoute
-          exact
-          path="/users/:userId"
-          component={UserShowContainer}
-        />
-        {/* </section> */}
+      <Route exact path="/restaurants" component={RestaurantIndexContainer} />
+      <ScrollToTop>
         <Route
           exact
-          path="/restaurants/:restaurantId"
-          component={RestaurantShowContainer}
-        ></Route>
+          path="/search-restaurants"
+          component={RestaurantSearchContainer}
+        />
+      </ScrollToTop>
+      {/* <Route exact path="/search/:query" component={RestaurantIndexContainer} /> */}
+      <ProtectedRoute
+        exact
+        path="/users/:userId/reservations"
+        component={ReservationIndexContainer}
+      />
+      <ProtectedRoute
+        exact
+        path="/users/:userId/favorites"
+        component={FavoritesIndexContainer}
+      />
+      <ProtectedRoute
+        exact
+        path="/users/:userId"
+        component={UserShowContainer}
+      />
+      {/* </section> */}
+      <Route
+        exact
+        path="/restaurants/:restaurantId"
+        component={RestaurantShowContainer}
+      ></Route>
 
-        <Footer />
-      </ErrorBoundary>
+      <Footer />
     </>
   );
 };

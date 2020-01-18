cd970495b319f77d95afa4df20dd25fb8047de3e
Gary <gdominguez210@live.com>
2020-01-17 22:33:43 -0500

    clean up restaurant banner syntax/formatting

--- frontend/components/restaurant/restaurant_show/restaurant_banner.jsx
+++ frontend/components/restaurant/restaurant_show/restaurant_banner.jsx
@@ -4,115 +4,93 @@ import { CSSTransition } from "react-transition-group";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const RestaurantBanner = props => {
-  const {
-    restaurant,
-    deleteRestaurant,
-    currentUser,
-    deleteFavorite,
-    createFavorite
-  } = props;
+  const { restaurant, deleteRestaurant, currentUser, deleteFavorite, createFavorite } = props;
 
   let banner;
-  if (restaurant.image_url) {
-    banner = {
-      backgroundImage: `url(${restaurant.image_url})`
-    };
-  } else {
-    banner = {};
-  }
+  restaurant.image_url ? banner = { backgroundImage: `url(${restaurant.image_url})` } : banner = {};
+
   const manageFavoriteOptions = () => {
     let result;
-    if (currentUser) {
-      if (restaurant.favorite_ids) {
-        if (
-          currentUser.favorite_ids.some(el =>
-            restaurant.favorite_ids.includes(el)
-          )
-        ) {
-          let favorite_id = currentUser.favorite_ids.find(el =>
-            restaurant.favorite_ids.includes(el)
-          );
-          result = (
-            <div className="button-container">
-              <button
-                className="readon danger"
-                onClick={() => deleteFavorite(favorite_id, restaurant.id)}
-              >
-                <span className="icon">
-                  <FontAwesomeIcon icon="times" />
-                </span>
-                Delete Favorite
-              </button>
-            </div>
-          );
-        } else {
-          result = (
-            <div className="button-container">
-              <button
-                className="readon plain"
-                onClick={() =>
-                  createFavorite({
-                    user_id: currentUser.id,
-                    restaurant_id: restaurant.id
-                  })
-                }
-              >
-                <span className="icon">
-                  <FontAwesomeIcon icon="bookmark" />
-                </span>
-                Favorite this Restaurant
-              </button>
-            </div>
-          );
-        }
-        return result;
-      }
-    }
-  };
-  const manageRestaurantOptions = () => {
-    let result;
-    if (currentUser) {
-      if (currentUser.id === restaurant.owner_id) {
+    if (currentUser && restaurant.favorite_ids) {
+      if (currentUser.favorite_ids.some(el => restaurant.favorite_ids.includes(el))) {
+        let favorite_id = currentUser.favorite_ids.find(el =>
+          restaurant.favorite_ids.includes(el)
+        );
         result = (
           <div className="button-container">
             <button
               className="readon danger"
-              onClick={() =>
-                deleteRestaurant(restaurant.id).then(() =>
-                  this.history.push("/")
-                )
-              }
+              onClick={() => deleteFavorite(favorite_id, restaurant.id)}
             >
               <span className="icon">
                 <FontAwesomeIcon icon="times" />
-              </span>{" "}
-              Delete Restaurant
-            </button>
-            <Link to={`/restaurants/${restaurant.id}/edit`} className="readon">
+              </span>
+              Delete Favorite
+              </button>
+          </div>
+        );
+      } else {
+        result = (
+          <div className="button-container">
+            <button
+              className="readon plain"
+              onClick={() =>
+                createFavorite({
+                  user_id: currentUser.id,
+                  restaurant_id: restaurant.id
+                })
+              }
+            >
               <span className="icon">
-                <FontAwesomeIcon icon="cog" />
-              </span>{" "}
-              Edit Restaurant
-            </Link>
+                <FontAwesomeIcon icon="bookmark" />
+              </span>
+              Favorite this Restaurant
+              </button>
           </div>
         );
       }
-    } else {
-      result = null;
+      return result;
     }
+  };
+
+  const manageRestaurantOptions = () => {
+    let result;
+    currentUser && currentUser.id === restaurant.owner_id ?
+      result = (
+        <div className="button-container">
+          <button
+            className="readon danger"
+            onClick={() =>
+              deleteRestaurant(restaurant.id).then(() =>
+                this.history.push("/")
+              )
+            }
+          >
+            <span className="icon">
+              <FontAwesomeIcon icon="times" />
+            </span>{" "}
+            Delete Restaurant
+            </button>
+          <Link to={`/restaurants/${restaurant.id}/edit`} className="readon">
+            <span className="icon">
+              <FontAwesomeIcon icon="cog" />
+            </span>{" "}
+            Edit Restaurant
+            </Link>
+        </div>
+      ) :
+      result = null;
     return result;
   };
 
   return (
-    <>
-      <CSSTransition in={true} appear={true} timeout={300} classNames="fade">
-        <section className="restaurant-featured-image">
-          {manageRestaurantOptions()}
-          {manageFavoriteOptions()}
-          <div className="featured-img" style={banner}></div>
-        </section>
-      </CSSTransition>
-    </>
+    <CSSTransition in={true} appear={true} timeout={300} classNames="fade">
+      <section className="restaurant-featured-image">
+        {manageRestaurantOptions()}
+        {manageFavoriteOptions()}
+        <div className="featured-img" style={banner}></div>
+      </section>
+    </CSSTransition>
   );
 };
 

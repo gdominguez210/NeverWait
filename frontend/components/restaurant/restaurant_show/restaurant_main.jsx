125a1e3ae299ba5ca55c725f79fd201645b368ac
Gary <gdominguez210@live.com>
2020-01-17 22:46:06 -0500

    clean up restaurant main syntax/formatting

--- frontend/components/restaurant/restaurant_show/restaurant_main.jsx
+++ frontend/components/restaurant/restaurant_show/restaurant_main.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import RestaurantQuickLinks from "./restaurant_quicklinks";
 import RestaurantGallery from "../restaurant_gallery/restaurant_gallery";
-import RestaurantStars from "../restaurant_ratings/rating_stars";
-import ReviewIndex from "../../reviews/review_index_container";
 import RestaurantReviews from "../restaurant_ratings/restaurant_reviews";
 import RestaurantHeader from "./restaurant_header";
-import CreateRestaurantForm from "../restaurant_forms/create_form_container";
-import { Route, Link } from "react-router-dom";
 import RestaurantSidebar from "./restaurant_sidebar";
 import MediaQuery from "react-responsive";
-import LazyLoad from "react-lazyload";
 import renderLoader from "../../loader/loader";
 
 class RestaurantMain extends React.Component {
@@ -21,7 +15,6 @@ class RestaurantMain extends React.Component {
       activeItem: { id: null, ratio: 0 },
       loading: true
     };
-    this.handleClick = this.handleClick.bind(this);
     this.rootRef = React.createRef();
     this.overview = React.createRef();
     this.photos = React.createRef();
@@ -31,20 +24,15 @@ class RestaurantMain extends React.Component {
       photos: { ref: this.photos, ratio: 0, id: "photos" },
       reviews: { ref: this.reviews, ratio: 0, id: "reviews" }
     };
-    this.callback = this.callback.bind(this);
     this.observer = new IntersectionObserver(this.callback, {
       root: this.rootRef.current,
       threshold: new Array(101).fill(0).map((v, i) => i * 0.01)
     });
-    this.loadingDone = this.loadingDone.bind(this);
   }
 
-  loadingDone() {
-    this.setState({
-      loading: false
-    });
-  }
-  callback(entries) {
+  loadingDone = () => this.setState({ loading: false });
+
+  callback = (entries) => {
     for (let i = 0; i < entries.length; i++) {
       let entry = entries[i];
       this.singleRefs[entry.target.id].ratio = entry.intersectionRatio;
@@ -60,93 +48,55 @@ class RestaurantMain extends React.Component {
   }
 
   componentDidMount() {
-    Object.values(this.singleRefs).forEach(value => {
-      this.observer.observe(value.ref.current);
-    });
+    Object.values(this.singleRefs).forEach(value => { this.observer.observe(value.ref.current); });
   }
 
   componentWillUnmount() {
     this.observer.disconnect();
   }
 
-  handleClick(ref) {
+  handleClick = (ref) => {
     const yOffset = -65;
     const yOffsetHeader = -250;
     let y = null;
-    if (ref.current.id === "overview") {
-      y =
-        ref.current.getBoundingClientRect().top +
-        window.pageYOffset +
-        yOffsetHeader;
-    } else {
-      y =
-        ref.current.getBoundingClientRect().top + window.pageYOffset + yOffset;
-    }
+    ref.current.id === "overview" ?
+      y = ref.current.getBoundingClientRect().top + window.pageYOffset + yOffsetHeader :
+      y = ref.current.getBoundingClientRect().top + window.pageYOffset + yOffset;
     window.scrollTo({ top: y, behavior: "smooth" });
   }
   render() {
     return (
-      <>
-        <main className="restaurant-content" ref={this.rootRef}>
-          {/* <RestaurantQuickLinks /> */}
-          <div
-            className={
-              this.state.sticky
-                ? "restaurant-quick-links-wrap sticky"
-                : "restaurant-quick-links-wrap"
-            }
-          >
-            <ul className="restaurant-quick-links">
-              <li
-                onClick={() => this.handleClick(this.overview)}
-                className={
-                  this.state.activeItem.id === "overview" ? "active" : null
-                }
-              >
-                Overview
+      <main className="restaurant-content" ref={this.rootRef}>
+        <div className={this.state.sticky ? "restaurant-quick-links-wrap sticky" : "restaurant-quick-links-wrap"}>
+          <ul className="restaurant-quick-links">
+            <li onClick={() => this.handleClick(this.overview)} className={this.state.activeItem.id === "overview" ? "active" : null}>
+              Overview
               </li>
-              <li
-                onClick={() => this.handleClick(this.photos)}
-                className={
-                  this.state.activeItem.id === "photos" ? "active" : null
-                }
-              >
-                Photos
+            <li onClick={() => this.handleClick(this.photos)} className={this.state.activeItem.id === "photos" ? "active" : null}>
+              Photos
               </li>
-              {/* <li onClick={() => this.handleClick(this.menu)}>Menu</li> */}
-              <li
-                onClick={() => this.handleClick(this.reviews)}
-                className={
-                  this.state.activeItem.id === "reviews" ? "active" : null
-                }
-              >
-                Reviews
+            <li onClick={() => this.handleClick(this.reviews)} className={this.state.activeItem.id === "reviews" ? "active" : null}>
+              Reviews
               </li>
-            </ul>
+          </ul>
+        </div>
+        <section className="restaurant-content-inner">
+          <div id="overview" ref={this.overview}>
+            <RestaurantHeader restaurant={this.props.restaurant} />
+            <MediaQuery maxWidth={1050}>
+              <RestaurantSidebar restaurant={this.props.restaurant} />
+            </MediaQuery>
+            <p>{this.props.restaurant.description}</p>
           </div>
-          <section className="restaurant-content-inner">
-            <div id="overview" ref={this.overview}>
-              <RestaurantHeader restaurant={this.props.restaurant} />
-              <MediaQuery maxWidth={1050}>
-                <RestaurantSidebar restaurant={this.props.restaurant} />
-              </MediaQuery>
-              <p>{this.props.restaurant.description}</p>
-            </div>
-            <div id="photos" ref={this.photos}>
-              {renderLoader(this.state)}
-              {this.state.loading ? null : (
-                <RestaurantGallery restaurant={this.props.restaurant} />
-              )}
-            </div>
-            <div id="reviews" ref={this.reviews}>
-              <RestaurantReviews
-                restaurant={this.props.restaurant}
-                loadingDone={this.loadingDone}
-              />
-            </div>
-          </section>
-        </main>
-      </>
+          <div id="photos" ref={this.photos}>
+            {renderLoader(this.state)}
+            {this.state.loading ? null : (<RestaurantGallery restaurant={this.props.restaurant} />)}
+          </div>
+          <div id="reviews" ref={this.reviews}>
+            <RestaurantReviews restaurant={this.props.restaurant} loadingDone={this.loadingDone} />
+          </div>
+        </section>
+      </main>
     );
   }
 }

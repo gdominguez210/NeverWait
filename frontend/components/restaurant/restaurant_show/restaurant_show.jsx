9b996028e1d47cfdabc10b72f616a35b4073549a
Gary <gdominguez210@live.com>
2020-01-18 22:44:33 -0500

    Clean up syntax of Restaurant Show

--- frontend/components/restaurant/restaurant_show/restaurant_show.jsx
+++ frontend/components/restaurant/restaurant_show/restaurant_show.jsx
@@ -1,25 +1,18 @@
 import React, { createRef } from "react";
-import { Link } from "react-router-dom";
 import RestaurantBanner from "./restaurant_banner";
 import RestaurantMain from "./restaurant_main";
 import RestaurantSidebar from "./restaurant_sidebar";
-import { CSSTransition } from "react-transition-group";
 import MediaQuery from "react-responsive";
 
 class RestaurantShow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      is_Mounted: false,
-      receivedReviews: false
-    };
+    this.state = { is_Mounted: false, receivedReviews: false };
   }
 
   componentDidMount() {
     this.setState({
-      restaurant: this.props.fetchRestaurant(
-        this.props.match.params.restaurantId
-      ),
+      restaurant: this.props.fetchRestaurant(this.props.match.params.restaurantId),
       is_Mounted: true
     });
   }
@@ -30,28 +23,14 @@ class RestaurantShow extends React.Component {
     const reviewsId = this.props.restaurant.review_ids;
     const fetchRestaurant = this.props.fetchRestaurant;
 
-     ;
     if (this.state.is_Mounted) {
       if (restaurantId !== prevProps.match.params.restaurantId) {
         fetchRestaurant(this.props.match.params.restaurantId);
-      } else if (
-        favoritesId.length !== prevProps.restaurant.favorite_ids.length
-      ) {
+      } else if (favoritesId.length !== prevProps.restaurant.favorite_ids.length) {
         fetchRestaurant(this.props.match.params.restaurantId);
-        // } else if (reviewsId.length !== prevProps.restaurant.review_ids.length) {
-      } else if (
-        Object.values(prevProps.reviews).length !==
-        Object.values(this.props.reviews).length
-      ) {
-        if (this.state.receivedReviews === false) {
-          this.setState({
-            receivedReviews: true
-          });
-        }
-        if (this.state.receivedReviews) {
-           ;
-          fetchRestaurant(this.props.match.params.restaurantId);
-        }
+      } else if (Object.values(prevProps.reviews).length !== Object.values(this.props.reviews).length) {
+        if (this.state.receivedReviews === false) this.setState({ receivedReviews: true });
+        if (this.state.receivedReviews) fetchRestaurant(this.props.match.params.restaurantId);
       }
     }
   }
@@ -59,23 +38,15 @@ class RestaurantShow extends React.Component {
   render() {
     const restaurant = this.props.restaurant;
     return (
-      <>
-        <section className="restaurant-container">
-          <RestaurantBanner
-            deleteRestaurant={this.props.deleteRestaurant}
-            restaurant={restaurant}
-            currentUser={this.props.currentUser}
-            createFavorite={this.props.createFavorite}
-            deleteFavorite={this.props.deleteFavorite}
-          />
-          <section className="restaurant-content-wrap">
-            <RestaurantMain restaurant={restaurant} />
-            <MediaQuery minWidth={1051}>
-              <RestaurantSidebar restaurant={restaurant} />
-            </MediaQuery>
-          </section>
+      <section className="restaurant-container">
+        <RestaurantBanner deleteRestaurant={this.props.deleteRestaurant} restaurant={restaurant} currentUser={this.props.currentUser} createFavorite={this.props.createFavorite} deleteFavorite={this.props.deleteFavorite} />
+        <section className="restaurant-content-wrap">
+          <RestaurantMain restaurant={restaurant} />
+          <MediaQuery minWidth={1051}>
+            <RestaurantSidebar restaurant={restaurant} />
+          </MediaQuery>
         </section>
-      </>
+      </section>
     );
   }
 }
